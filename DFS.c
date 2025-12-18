4714ba51e216b50c4496aa2ec916ab64eb2f3111
dhyansirigerics24 <dhyansirigeri.cs24@bmsce.ac.in>
2025-12-18 11:05:57 +0530

    DFS.c

--- DFS.c
+++ DFS.c
@@ -1,82 +1,81 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Node structure for adjacency list */
 struct node {
     int vertex;
-    struct node *next;
+    struct node* next;
 };
 
-struct node *adj[20];
-int visited[20], n, end, found = 0;
+struct node* adjList[20];
+int visited[20];
+int n;
 
-struct node* createNode(int v)
-{
-    struct node *newNode = (struct node*)malloc(sizeof(struct node));
+/* Create a new node */
+struct node* createNode(int v) {
+    struct node* newNode = (struct node*)malloc(sizeof(struct node));
     newNode->vertex = v;
     newNode->next = NULL;
     return newNode;
 }
 
-void addEdge(int src, int dest)
-{
-    struct node *newNode = createNode(dest);
-    newNode->next = adj[src];
-    adj[src] = newNode;
+/* Add edge (undirected graph) */
+void addEdge(int src, int dest) {
+    struct node* newNode = createNode(dest);
+    newNode->next = adjList[src];
+    adjList[src] = newNode;
+
+    newNode = createNode(src);
+    newNode->next = adjList[dest];
+    adjList[dest] = newNode;
 }
 
-void dfs(int v)
-{
-    struct node *temp;
-
-    if (found)
-        return;
-
+/* DFS function */
+void DFS(int v) {
+    struct node* temp;
     visited[v] = 1;
-    printf("%d ", v);
 
-    if (v == end)
-    {
-        found = 1;
-        return;
-    }
-
-    temp = adj[v];
-    while (temp != NULL)
-    {
-        if (!visited[temp->vertex])
-            dfs(temp->vertex);
+    temp = adjList[v];
+    while (temp != NULL) {
+        if (visited[temp->vertex] == 0) {
+            DFS(temp->vertex);
+        }
         temp = temp->next;
     }
 }
 
-int main()
-{
-    int i, e, src, dest, start;
+int main() {
+    int i, edges, src, dest;
 
     printf("Enter number of vertices: ");
     scanf("%d", &n);
 
-    for (i = 1; i <= n; i++)
-        adj[i] = NULL;
+    /* Initialize adjacency list and visited array */
+    for (i = 0; i < n; i++) {
+        adjList[i] = NULL;
+        visited[i] = 0;
+    }
 
     printf("Enter number of edges: ");
-    scanf("%d", &e);
+    scanf("%d", &edges);
 
-    printf("Enter source and destination edges:\n");
-    for (i = 0; i < e; i++)
-    {
+    printf("Enter edges (source destination):\n");
+    for (i = 0; i < edges; i++) {
         scanf("%d %d", &src, &dest);
         addEdge(src, dest);
     }
 
-    printf("Enter starting vertex: ");
-    scanf("%d", &start);
+    /* Start DFS from vertex 0 */
+    DFS(0);
 
-    printf("Enter ending vertex: ");
-    scanf("%d", &end);
+    /* Check if all vertices are visited */
+    for (i = 0; i < n; i++) {
+        if (visited[i] == 0) {
+            printf("\nThe graph is NOT connected.\n");
+            return 0;
+        }
+    }
 
-    printf("DFS Traversal: ");
-    dfs(start);
-
+    printf("\nThe graph is CONNECTED.\n");
     return 0;
 }
